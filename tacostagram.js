2775e8cca4a2d53d66c16e0a5e8b9abb521e4548
SportMode Code <james.epperson@kellogg.northwestern.edu>
2024-03-07 01:37:07 +0000

    break 1 update

--- tacostagram.js
+++ tacostagram.js
@@ -1,6 +1,9 @@
 function getPostsFromAPI() {
   // Replace this with the URL of the JSON API that returns an array of image URLs
-  const url = 'YOUR_URL_GOES_HERE';
+  const url = 'https://3000-sportmodeco-tacogramfin-tcpihd9bqxd.ws-us108.gitpod.io/posts.json';
+  if (url == 'YOUR_URL_GOES_HERE') {
+    alert('Error: Replace url value in tacostagram.js')
+  }
 
   // Make a GET request to the API
   fetch(url)
@@ -9,9 +12,15 @@ function getPostsFromAPI() {
       // Loop through the array of posts and build html for each
       for (let post of posts) {
         console.log(post);
+        // alert("heyyyyy")
 
         // html for each post goes here
-        let html = ``;
+        let html = `
+          <div class="col-md-6 col-lg-3">
+            <img src="${post["image"]}" class="img-fluid">
+            <p>${post["body"]}</p>
+          </div>
+        `;
 
         const postsDiv = document.querySelector('#posts');
         postsDiv.innerHTML += html
